e17ee6c8fc8b900333c101f4d69537abfae87251
Smark16 <sengendomark16@gmail.com>
2025-02-15 17:09:02 +0300

    whitenoise

--- backend/settings.py
+++ backend/settings.py
@@ -69,6 +69,7 @@ FLUTTERWAVE_SECRET_KEY = 'FLWSECK_TEST-823dbcdbcd999c1ce7d67270d628d090-X'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
@@ -155,6 +156,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
